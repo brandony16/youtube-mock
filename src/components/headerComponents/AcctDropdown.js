d063f01bf2c990f32e4a7809f492959d31624ba0
BabySparta <123456789+odin@users.noreply.github.com>
2023-07-17 14:13:55 -0400

    Add sign out functionality

---
+++ src/components/headerComponents/AcctDropdown.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { auth } from "../../firebase/firebaseConfig";
+import "../../styles/componentStyles/AcctDropdown.css";
+
+const AcctDropdown = ({ user }) => {
+  const handleSignOut = async () => {
+    try {
+      await auth.signOut();
+    } catch (error) {
+      console.error("Error signing out:", error);
+    }
+  };
+
+  return (
+    <div className="acctInner">
+      <div className="acctItem topItem">
+        <img src={user.photoURL} alt={user.displayName} className="acctImg" />
+        <div className="nameWrap">
+          <h3 className="username">{user.displayName}</h3>
+          <p className="email">{user.email}</p>
+        </div>
+      </div>
+      <div className="acctItem">
+        <div className="acctOption" onClick={handleSignOut}>
+          <span class="material-symbols-outlined thin">login</span>
+          <p className="signOut">Sign Out</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AcctDropdown;
--- src/components/headerComponents/RightUser.js
+++ src/components/headerComponents/RightUser.js
@@ -1,6 +1,27 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import AcctDropdown from "./AcctDropdown";
 
 const RightUser = ({ user }) => {
+  const [acctVisible, setAcctVisible] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const toggleAcctVisible = () => {
+    setAcctVisible((prevState) => !prevState);
+  };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target) && !event.target.classList.contains("userIcon")) {
+      setAcctVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("click", handleClickOutside);
+    return () => {
+      document.removeEventListener("click", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="rightHeader userInfo">
       <div className="tooltip" data-tooltip="Create">
@@ -13,7 +34,17 @@ const RightUser = ({ user }) => {
           notifications
         </span>
       </div>
-      <img src={user.photoURL} alt="user" className="userIcon filled"/>
+      <img
+        src={user.photoURL}
+        alt={user.displayName}
+        className="userIcon filled"
+        onClick={toggleAcctVisible}
+      />
+      {acctVisible && (
+          <div className="acctDropdown" ref={dropdownRef}>
+            <AcctDropdown user={user}/>
+          </div>
+      )}
     </div>
   );
 };
